720bfe1ba6a2ffb8b2ee30ddfe66134ba3419188
elystanz <elysestanziale@gmail.com>
2022-03-23 21:20:32 -0400

    improve CSS

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// have questions stored in an array
+// use radio buttons to input numerical information 1-5
+// have a "next" button once question is answered
+// once all questions have been cycled through, have a "finish" button to provide results
+// display results
+// have input field for name that will allow results to be saved to local storage
+// have a button to access a new page that lists results
